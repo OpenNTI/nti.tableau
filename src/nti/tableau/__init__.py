fd5c85652a9362d11c4ec2dc694b3e58ffc86a54
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-30 21:41:03 -0500

    save

--- src/nti/tableau/__init__.py
+++ src/nti/tableau/__init__.py
@@ -9,3 +9,5 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 API_VERSION = u'2.8'
+
+### from gevent.event import Event; import threading; threading._Event = Event; from IPython.terminal.debugger import set_trace;set_trace()
